54d523b077d1b7b270b6753d9d373ce3bbec5a50
Colin Chargy <colin.chargy@gmail.com>
2015-08-13 21:08:26 +0000

    Finishing retreiving all sensor data from Roomba

--- main.py
+++ main.py
@@ -89,36 +89,78 @@ if True:
 			if res:
 				acc += struct.unpack('B', res)
 		
-		print acc
-		
 		Infos = {}
 		Infos['Bumps'] = {}
+		Infos['Cliff'] = {}
+		Infos['Cliff']['Presence'] = {}
+		Infos['Cliff']['Signal'] = {}
+		Infos['Battery'] = {}
+		Infos['Motor current'] = {}
+		Infos['Motor current']['Overcurrents'] = {}
+		Infos['Motor current']['Signal'] = {}
+		Infos['Wall'] = {}
+		Infos['Wheel drop'] = {}
+		Infos['Movement'] = {}
+		Infos['Light bump'] = {}
+		Infos['Misc'] = {}
+		Infos['Useless'] = {}
+		
 		Infos['Bumps']['Left'] = bool(acc[0] & 0x02)
 		Infos['Bumps']['Right'] = bool(acc[0] & 0x01)
-		Infos['Wheel drop'] = {}
+		
 		Infos['Wheel drop']['Right'] = bool(acc[0] & 0x04)
 		Infos['Wheel drop']['Left'] = bool(acc[0] & 0x08)
-		Infos['Cliff'] = {}
-		Infos['Cliff']['Left'] = bool(acc[2])
-		Infos['Cliff']['Front left'] = bool(acc[3])
-		Infos['Cliff']['Front right'] = bool(acc[4])
-		Infos['Cliff']['Right'] = bool(acc[5])
-		Infos['Wall'] = bool(acc[1])
-		Infos['Wheel overcurrents'] = {}
-		Infos['Wheel overcurrents']['Side brush'] = bool(acc[7] & 0x01)
-		Infos['Wheel overcurrents']['Main brush'] = bool(acc[7] & 0x04)
-		Infos['Wheel overcurrents']['Right wheel'] = bool(acc[7] & 0x08)
-		Infos['Wheel overcurrents']['Left wheel'] = bool(acc[7] & 0x10)
-		Infos['Dirt detect'] = bool(acc[8])
-		Infos['Distance'] = DecodeSignedShort(acc[12], acc[13])
-		Infos['Angle'] = DecodeSignedShort(acc[14], acc[15])
-		Infos['Charging state'] = {0: 'Not charging', 1: 'Reconditioning Charging', 2: 'Full Charging', 3: 'Trickle Charging', 4: 'Waiting', 5: 'Charging Fault Condition'}[acc[16]]
-		Infos['Voltage'] = DecodeUnsignedShort(acc[17], acc[18])
-		Infos['Current'] = DecodeSignedShort(acc[19], acc[20])
-		Infos['Temperature'] = acc[21]
+		
+		Infos['Cliff']['Presence']['Left'] = bool(acc[2])
+		Infos['Cliff']['Presence']['Front left'] = bool(acc[3])
+		Infos['Cliff']['Presence']['Front right'] = bool(acc[4])
+		Infos['Cliff']['Presence']['Right'] = bool(acc[5])
+		
+		Infos['Cliff']['Signal']['Left'] = DecodeUnsignedShort(acc[28], acc[29])
+		Infos['Cliff']['Signal']['Front left'] = DecodeUnsignedShort(acc[30], acc[31])
+		Infos['Cliff']['Signal']['Front right'] = DecodeUnsignedShort(acc[32], acc[33])
+		Infos['Cliff']['Signal']['Right'] = DecodeUnsignedShort(acc[34], acc[35])
+		
+		Infos['Motor current']['Overcurrents']['Side brush'] = bool(acc[7] & 0x01)
+		Infos['Motor current']['Overcurrents']['Main brush'] = bool(acc[7] & 0x04)
+		Infos['Motor current']['Overcurrents']['Right wheel'] = bool(acc[7] & 0x08)
+		Infos['Motor current']['Overcurrents']['Left wheel'] = bool(acc[7] & 0x10)
+		Infos['Motor current']['Signal']['Side brush'] = DecodeSignedShort(acc[76], acc[77])
+		Infos['Motor current']['Signal']['Main brush'] = DecodeSignedShort(acc[74], acc[75])
+		Infos['Motor current']['Signal']['Right wheel'] = DecodeSignedShort(acc[72], acc[73])
+		Infos['Motor current']['Signal']['Left wheel'] = DecodeSignedShort(acc[70], acc[71])
+		
+		Infos['Battery']['Charging state'] = {0: 'Not charging', 1: 'Reconditioning Charging', 2: 'Full Charging', 3: 'Trickle Charging', 4: 'Waiting', 5: 'Charging Fault Condition'}[acc[16]]
+		Infos['Battery']['Charge'] = DecodeUnsignedShort(acc[22], acc[23])
+		Infos['Battery']['Capacity'] = DecodeUnsignedShort(acc[24], acc[25])
+		Infos['Battery']['Charger available'] = {'Home base' : bool(acc[38] & 0x02), 'Internal charger' : bool(acc[38] & 0x01)}
+		Infos['Battery']['Voltage'] = DecodeUnsignedShort(acc[17], acc[18])
+		Infos['Battery']['Current'] = DecodeSignedShort(acc[19], acc[20])
+		
+		Infos['Wall']['Presence'] = bool(acc[1])
+		Infos['Wall']['Signal'] = DecodeUnsignedShort(acc[26], acc[27])
+		
+		Infos['Movement']['Distance'] = DecodeSignedShort(acc[12], acc[13])
+		Infos['Movement']['Angle'] = DecodeSignedShort(acc[14], acc[15])
+		Infos['Movement']['Velocity'] = {'Global' : DecodeSignedShort(acc[43], acc[44]), 'Right' : DecodeSignedShort(acc[49], acc[50]), 'Left' : DecodeSignedShort(acc[47], acc[48])}
+		Infos['Movement']['Radius'] = DecodeSignedShort(acc[45], acc[46])
+		Infos['Movement']['Encoder counts'] = {'Left' : DecodeUnsignedShort(acc[51], acc[52]), 'Right' : DecodeUnsignedShort(acc[53], acc[54])}
+		Infos['Movement']['Stasis'] = bool(acc[78])
+		
+		Infos['Light bump']['Presence'] = {'Left' : bool(acc[55] & 0x01), 'Front Left' : bool(acc[55] & 0x02), 'Center Left' : bool(acc[55] & 0x04), 'Center Right' : bool(acc[55] & 0x08), 'Front Right' : bool(acc[55] & 0x10), 'Right' : bool(acc[55] & 0x20)}
+		Infos['Light bump']['Signal'] = {'Left' : DecodeUnsignedShort(acc[56], acc[57]), 'Front Left' : DecodeUnsignedShort(acc[58], acc[59]), 'Center Left' : DecodeUnsignedShort(acc[60], acc[61]), 'Center Right' : DecodeUnsignedShort(acc[62], acc[63]), 'Front Right' : DecodeUnsignedShort(acc[64], acc[65]), 'Right' : DecodeUnsignedShort(acc[66], acc[67])}
+		
+		Infos['Misc']['Dirt detect'] = bool(acc[8])
+		Infos['Misc']['Temperature'] = acc[21]
+		Infos['Misc']['Mode'] = {0: 'Off', 1: 'Passive', 2: 'Safe', 3: 'Full'}[acc[39]]
+		Infos['Misc']['Song'] = {'Number' : acc[40], 'Playing' : bool(acc[41])}
+		Infos['Misc']['Buttons'] = acc[11]
+		
+		Infos['Useless']['Packet size'] = acc[42]
+		Infos['Useless']['Infrared optcode'] = {'Global' : acc[10], 'Left' : acc[68], 'Right' : acc[69]}
 		
 		print 'Received : ' + str(Infos)
-		time.sleep(1)
+		time.sleep(10)
 	
 # except:
 	# print 'Error detected : ' + str(sys.exc_info()[0])
